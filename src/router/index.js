05bb10d58e46e9f3731c9691ab8db5f0bbe4eb5d
Malte <Malte.Krolik@hs-furtwangen.de>
2019-11-26 20:48:30 +0100

    index.js aus Demo kopiert

--- src/router/index.js
+++ src/router/index.js
@@ -1,76 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from "../views/Login";
-import Startseite from "../views/Startseite";
-import Menü from "../views/Menü";
-import Profil from "../views/Profil";
-import Hilfe from "../views/Hilfe";
-import findFriends from "../views/findFriends";
-import PinSetzen from "../views/PinSetzen";
-import addFoto from "../views/addFoto";
-import Ticket1 from "../views/Ticket1";
-import Ticket2 from "../views/Ticket2";
-import TicketLaden from "../views/TicketLaden";
+import Login from '../views/Login'
+import Order from '../views/Order'
+import Overview from '../views/Overview'
+import App from '../App'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
     routes: [
         {
-            path: '/Login',
-            name: "Login",
+            path: '/',
+            name: 'Root',
+            component: App
+        },
+        {
+            path: '/overview',
+            name: 'Overview',
+            component: Overview
+        },
+        {
+            path: '/login',
+            name: 'Login',
             component: Login
         },
         {
-            path: '/Startseite',
-            name: "Startseite",
-            component: Startseite
+            path: '/order',
+            name: 'Order',
+            component: Order
         },
-        {
-            path: '/Menü',
-            name: "Menü",
-            components: Menü
-        },
-        {
-            path: '/Profil',
-            name: "Profil",
-            components: Profil
-        },
-        {
-            path: '/Hilfe',
-            name: "Hilfe",
-            components: Hilfe
-        },
-        {
-            path: '/findFriends',
-            name: "findFriends",
-            components: findFriends
-        },
-        {
-            path: '/PinSetzen',
-            name: "PinSetzen",
-            components: PinSetzen
-        },
-        {
-            path: '/addFoto',
-            name:"addFoto",
-            components: addFoto
-        },
-        {
-            path: '/Ticket1',
-            name: "Ticket1",
-            components: Ticket1
-        },
-        {
-            pathe: '/Ticket2',
-            name: "Ticket2",
-            components: Ticket2
-        },
-        {
-            path: '/TicketLaden',
-            name: "TicketLaden",
-            components: TicketLaden
-        },
-
     ]
 })
