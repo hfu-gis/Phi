a41ecd5e5923b2aaa40d200cba44e3e0b604eb2f
LeonieSchwall <56916271+LeonieSchwall@users.noreply.github.com>
2019-12-29 18:22:45 +0100

    interaktive Weltkarte/ Länder benannt

--- src/views/PinSetzen.vue
+++ src/views/PinSetzen.vue
@@ -2,20 +2,15 @@
     <div class="body-1">
         <!-- HTML-Code aus dem Body-Tag hier einfügen -->
 
-
+        <div class="tooltip">Hover over me
+            <span class="tooltiptext">Tooltip text</span>
+        </div>
         <header><h1 class="h1">Länder suchen</h1></header>
         <header><h2 class="h2">wähle Wunschländer aus</h2></header>
 
         <input type="text" placeholder="Search..">
 
-        <h2>Länderliste</h2>
 
-        <ul style="list-style-type:disc;">
-            <li>Australien</li>
-            <li>Argentinien</li>
-            <li>Belgien</li>
-            <li>...</li>
-        </ul>
 
         <div class='pin bounce'></div>
         <div class='pulse'></div>
@@ -58,92 +53,50 @@
         background: #e6e6e6;
     }
 
-    .pin {
-        width: 30px;
-        height: 30px;
-        border-radius: 50% 50% 50% 0;
-        background: #00cae9;
-        position: absolute;
-        transform: rotate(-45deg);
-        left: 50%;
-        top: 50%;
-        margin: -20px 0 0 -20px;
-    }
-    .pin:after {
-        content: "";
-        width: 14px;
-        height: 14px;
-        margin: 8px 0 0 8px;
-        background: #e6e6e6;
-        position: absolute;
-        border-radius: 50%;
+    /* Tooltip container */
+    .tooltip {
+        position: relative;
+        display: inline-block;
+        border-bottom: 1px dotted black; /* If you want dots under the hoverable text */
     }
 
-    .bounce {
-        animation-name: bounce;
-        animation-fill-mode: both;
-        animation-duration: 1s;
-    }
+    /* Tooltip text */
+    .tooltip .tooltiptext {
+        visibility: hidden;
+        width: 120px;
+        background-color: #555;
+        color: #fff;
+        text-align: center;
+        padding: 5px 0;
+        border-radius: 6px;
 
-    .pulse {
-        background: #d6d4d4;
-        border-radius: 50%;
-        height: 14px;
-        width: 14px;
+        /* Position the tooltip text */
         position: absolute;
+        z-index: 1;
+        bottom: 125%;
         left: 50%;
-        top: 50%;
-        margin: 11px 0px 0px -12px;
-        transform: rotateX(55deg);
-        z-index: -2;
-    }
-    .pulse:after {
-        content: "";
-        border-radius: 50%;
-        height: 40px;
-        width: 40px;
-        position: absolute;
-        margin: -13px 0 0 -13px;
-        animation: pulsate 1s ease-out;
-        animation-iteration-count: infinite;
+        margin-left: -60px;
+
+        /* Fade in tooltip */
         opacity: 0;
-        box-shadow: 0 0 1px 2px #00cae9;
-        animation-delay: 1.1s;
+        transition: opacity 0.3s;
     }
 
-    @keyframes pulsate {
-        0% {
-            transform: scale(0.1, 0.1);
-            opacity: 0;
-        }
-
-        50% {
-            opacity: 1;
-        }
-
-        100% {
-            transform: scale(1.2, 1.2);
-            opacity: 0;
-        }
+    /* Tooltip arrow */
+    .tooltip .tooltiptext::after {
+        content: "";
+        position: absolute;
+        top: 100%;
+        left: 50%;
+        margin-left: -5px;
+        border-width: 5px;
+        border-style: solid;
+        border-color: #555 transparent transparent transparent;
     }
 
-    @keyframes bounce {
-        0% {
-            opacity: 0;
-            transform: translateY(-2000px) rotate(-45deg);
-        }
-
-        60% {
-            opacity: 1;
-            transform: translateY(30px) rotate(-45deg);
-        }
-
-        80% {
-            transform: translateY(-10px) rotate(-45deg);
-        }
-
-        100% {
-            transform: translateY(0) rotate(-45deg);
-        }
+    /* Show the tooltip text when you mouse over the tooltip container */
+    .tooltip:hover .tooltiptext {
+        visibility: visible;
+        opacity: 1;
     }
 </style>
