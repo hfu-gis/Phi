cd1e88ba5df47a48b2fb4437b19d1b438fcee370
isabe <isabel.schindler@hs-furtwangen.de>
2020-01-07 13:08:25 +0100

    Feed vue verändert

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -1,44 +1,79 @@
 <template>
+
+
     <v-card
+
+            max-width="1000"
+
             class="mx-auto"
-            max-width="400"
+
     >
+
+        <v-list-item>
+
+            <v-list-item-avatar color="grey"></v-list-item-avatar>
+
+            <v-list-item-content>
+
+                <v-list-item-title class="headline">Lisa Wagner</v-list-item-title>
+
+
+            </v-list-item-content>
+
+        </v-list-item>
+
         <v-img
                 class="white--text align-end"
-                height="200px"
+                height="500px"
                 src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
         >
-            <v-card-title>Top 10 Australian beaches</v-card-title>
+            <v-card-title>My top 10 Australian beaches</v-card-title>
+
         </v-img>
 
-        <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
 
-        <v-card-text class="text--primary">
-            <div>Whitehaven Beach</div>
 
-            <div>Whitsunday Island, Whitsunday Islands</div>
+        <v-card-text>
+            Australia has over 10.000 beaches. You could spend 27 years visiting another beach every single day. Of course I haven't seen them all, as I've only travelled Australia for one year, but I've seen a few. So here's a list of my top 10 Australian beaches.
+
         </v-card-text>
 
+
+
         <v-card-actions>
+
             <v-btn
-                    color="orange"
+
                     text
+
+                    color="blue"
+
             >
-                Explore
+
+                Read more
+
             </v-btn>
+
 
             <v-spacer></v-spacer>
 
             <v-btn icon>
+
                 <v-icon>mdi-heart</v-icon>
+
             </v-btn>
 
             <v-btn icon>
+
                 <v-icon>mdi-bookmark</v-icon>
+
             </v-btn>
 
         </v-card-actions>
+
     </v-card>
+
+
 </template>
 
 <script>
